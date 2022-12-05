12460a6c4874461def5ba97ce03fa1fe2e0b433d
kele-godwin <nnamokokele@yahoo.com>
2022-12-05 13:31:16 -0800

    truncate it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * create_file - Creates a file.
- * @filename: A pointer to the name of the file to create.
+ * create_file - Used to Create a file.
+ * @filename: A pointer to the name of the file to be  created.
  * @text_content: A pointer to a string to write to the file.
  *
  * Return: If the function fails - -1.
