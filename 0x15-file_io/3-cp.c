976c414ba570c4e9105f632c95e20eec89462bb5
kele-godwin <nnamokokele@yahoo.com>
2022-12-05 13:34:21 -0800

    new line, on the POSIX standard error

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,7 +6,7 @@ void close_file(int fd);
 /**
  * create_buffer - Allocates 1024 bytes for a buffer.
  * @file: The name of the file buffer is storing chars for.
- *
+ * Permissions of the created file: rw-rw-r--.
  * Return: A pointer to the newly-allocated buffer.
  */
 char *create_buffer(char *file)
