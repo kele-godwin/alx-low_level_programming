0669199b20711170d01c137716755a94ba7a0387
kele-godwin <nnamokokele@yahoo.com>
2022-12-05 13:39:12 -0800

    using read a 2nd time

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -20,7 +20,7 @@ void close_elf(int elf);
 /**
  * check_elf - Checks if a file is an ELF file.
  * @e_ident: A pointer to an array containing the ELF magic numbers.
- *
+ * Write a program that displays the information contained in the ELF header at the start of an ELF file.
  * Description: If the file is not an ELF file - exit code 98.
  */
 void check_elf(unsigned char *e_ident)
